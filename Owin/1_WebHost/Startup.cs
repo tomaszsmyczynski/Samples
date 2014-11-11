43281e7a9d7942426ca84071f0a32e9c941437e6
emiljuchnikowski <emil@juchnikowski.com>
2014-11-11 21:52:23 +0100

    Pierwsza wersja

---
+++ Owin/1_WebHost/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(_1_WebHost.Startup))]
+
+namespace _1_WebHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello, world.");
+            });
+        }
+    }
+}
