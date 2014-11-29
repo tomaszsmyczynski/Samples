35f8901394a638aaca6c854e6d8c37d33f2ec977
emiljuchnikowski <emil@juchnikowski.com>
2014-11-29 10:22:59 +0100

    Web API - pierwsza aplikacja

---
+++ Owin/2_WebApi/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+using _2_WebApi;
+
+[assembly: OwinStartup(typeof (Startup))]
+
+namespace _2_WebApi {
+    public class Startup {
+        public void Configuration(IAppBuilder app) {
+            var config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseWebApi(config);
+        }
+    }
+}
